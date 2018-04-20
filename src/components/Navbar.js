0abbb3e7aa4121efdee7a1cd15132435c033cf94
John Henson <jhenson@theworkshop.com>
2018-04-20 17:28:19 +0200

    Quick update of title + nav logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,10 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          // <figure className="image">
+          //   <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          // </figure>
+          Muy Bien Espanol
         </Link>
       </div>
       <div className="navbar-start">
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Learn Spanish Online | Muy Bien Espanol" />
     <Navbar />
     <div>{children()}</div>
   </div>
