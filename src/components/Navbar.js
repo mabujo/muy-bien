de4fd8f043368f0b9da285e3e8830aea2decf60e
John Henson <jhenson@theworkshop.com>
2018-04-20 17:31:56 +0200

    remove git link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,6 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          // <figure className="image">
-          //   <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          // </figure>
           Muy Bien Espanol
         </Link>
       </div>
@@ -24,16 +21,6 @@ const Navbar = () => (
         </Link>
       </div>
       <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
