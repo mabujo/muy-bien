fe41bf374774028523ab466bdc5e4f433999656d
John Henson <jhenson@theworkshop.com>
2018-04-23 14:19:20 +0200

    Fonts

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ import HeroStyles from '../styles/Hero.module.sass'
 const Hero = () => (
   <section id={HeroStyles.Hero}>
     <div className={HeroStyles.values}>
-      <h1 className={HeroStyles.proposition}>Hablar como un nativo</h1>
-      <h2 className={HeroStyles.tagline}>Mejora tu nivel de Español con classes individual online</h2>
+      <h1 className={`${HeroStyles.proposition} Brasley-Bold`}>Hablar como un nativo</h1>
+      <h2 className={`${HeroStyles.tagline} Brasley-Regular`}>Mejora tu nivel de Español con classes individual online</h2>
     </div>
   </section>
 )
